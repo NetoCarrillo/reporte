0cd7c25dc3290622189e987ccf23aeab2f7a1496
NetoCarrillo <ernesto_brouwer@hotmail.com>
2019-03-20 13:57:05 -0600

    All functional requirements maquettes

--- compile.sh
+++ compile.sh
@@ -5,7 +5,9 @@ FILE_NAME=$1
 echo $FILE_NAME
 
 latex  $FILE_NAME.tex
-bibtex $FILE_NAME
-latex  $FILE_NAME.tex
-latex  $FILE_NAME.tex
+if [[ $2 == 'b' ]]; then
+	bibtex $FILE_NAME
+	latex  $FILE_NAME.tex
+	latex  $FILE_NAME.tex
+fi
 dvipdfm $FILE_NAME.dvi
